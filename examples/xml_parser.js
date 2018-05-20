667f07268d5687806d1d8ec07415a774e610fd8e
Payne <admin@micblo.com>
2018-05-20 15:35:57 +0800

    fix: #unserializeXml(): Unexpected item object when length of item is 1

--- examples/xml_parser.js
+++ examples/xml_parser.js
@@ -2,30 +2,40 @@ const convert = require('xml-js');
 const xml =
 `<?xml version="1.0" encoding="ISO-8859-1"?>
 <root>
-	<item id="0">
-		<item id="uid">0</item>
-		<item id="username"><![CDATA[sdcefd423drfx]]></item>
-	</item>
 	<item id="1">
-		<item id="uid"><![CDATA[1]]></item>
-		<item id="username"><![CDATA[测试test123]]></item>
+		<item id="appid"><![CDATA[1]]></item>
+		<item id="type"><![CDATA[OTHER]]></item>
+		<item id="name"><![CDATA[测试]]></item>
+		<item id="url"><![CDATA[http://127.0.0.1:7001]]></item>
+		<item id="tagtemplates">
+			<item id="template"><![CDATA[]]></item>
+		</item>
+		<item id="viewprourl"><![CDATA[]]></item>
+		<item id="synlogin"><![CDATA[1]]></item>
 	</item>
 </root>`;
 const result1 = convert.xml2js(xml, {compact: true});
+console.log(convert.xml2json(xml, {compact: true}));
 
 function analyzeNode(node) {
     if (node.root) {
         return analyzeNode(node.root);
     } else if (node.item) {
-        if (/^\d+$/g.test(node.item[0]._attributes.id)) {
-            return node.item.map(v => {
-                return analyzeNode(v);
-            });
+        if (Array.isArray(node.item)) {
+            if (/^\d+$/g.test(node.item[0]._attributes.id)) {
+                return node.item.map(v => {
+                    return analyzeNode(v);
+                });
+            } else {
+                const data = {};
+                node.item.forEach(v => {
+                    data[v._attributes.id] = analyzeNode(v);
+                });
+                return data;
+            }
         } else {
             const data = {};
-            node.item.forEach(v => {
-                data[v._attributes.id] = analyzeNode(v);
-            });
+            data[node.item._attributes.id] = analyzeNode(node.item);
             return data;
         }
     } else if (node._cdata) {
